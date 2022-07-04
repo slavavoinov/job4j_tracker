00bcaa89c24252e121b28f4e9c424b346ebe70fd
slavavoinov <voijnov@yandex.ru>
2022-07-04 12:20:32 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Ball {
+
+    public void tryRun(boolean condition) {
+        if (condition) {
+            System.out.println("Kolobok sjeden");
+        } else {
+            System.out.println("Kolobok sbejal");
+        }
+    }
+}
