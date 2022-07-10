576090eea3cfbcfa60a98847e9660a0b26441fc5
slavavoinov <voijnov@yandex.ru>
2022-07-10 10:38:36 +0300

    7. Date. Отображение даты. [#310017 #320258]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -6,7 +6,7 @@ public class Item {
     private int id;
     private String name;
 
-    LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,3 +12,4 @@ public class StartUI {
         System.out.println(currentDateTimeFormat);
     }
 }
+
