8d17ac8668eea99c2537c893d4e2c6fe238d17e4
slavavoinov <voijnov@yandex.ru>
2022-07-16 10:35:43 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #322102]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -43,8 +43,7 @@ public class Tracker {
     public boolean delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
-            items[id] = null;
-            System.arraycopy(items, id + 1, items, id, size - id);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
             return true;
